fe2344f5aada8fd8e3f263a0d5c95f1d88f6beaf
CoderPlayer0 <2997385876@qq.com>
2020-12-31 16:19:54 +0800

    解决pom文件冲突

--- src/main/java/pojo/TreasureBox.java
+++ src/main/java/pojo/TreasureBox.java
@@ -1,4 +1,3 @@
-<<<<<<< .mine
 package pojo;
 
 import java.sql.Timestamp;
@@ -68,74 +67,3 @@ public class TreasureBox {
 				'}';
 	}
 }
-=======
-package pojo;
-
-import java.sql.Timestamp;
-
-/**
- * 宝箱对象
- *
- * @author liuYunCai
- * @date 2020/12/31 14:59
- **/
-public class TreasureBox {
-
-	private Integer id;
-
-	private Timestamp timestamp;
-
-	private String hashValue;
-
-	private String lastHashValue;
-
-	private int secretNum;
-
-	private String status;
-
-
-
-	@Override
-	public String toString() {
-		return "pojo.TreasureBox{" +
-				"id=" + id +
-				", timestamp=" + timestamp +
-				", hashValue='" + hashValue + '\'' +
-				", lastHashValue='" + lastHashValue + '\'' +
-				", secretNum=" + secretNum +
-				", status='" + status + '\'' +
-				'}';
-	}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> .theirs
